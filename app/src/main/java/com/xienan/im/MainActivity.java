4d6736baf2a126f1b8745d72f05954bd01533dab
xienan <xienan_ma43@126.com>
2016-01-29 17:02:15 +0800

    test

--- app/src/main/java/com/xienan/im/MainActivity.java
+++ app/src/main/java/com/xienan/im/MainActivity.java
@@ -1,13 +1,69 @@
 package com.xienan.im;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.xienan.im.view.MyVideoView;
+
 public class MainActivity extends Activity {
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private MyVideoView myVideoView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findView();
+
+        initSettings();
+
+        setListener();
+
+        myVideoView = (MyVideoView) findViewById(R.id.myvideoview);
+
+
+        myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.entrance_video));
+        myVideoView.start();
+        myVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                // TODO Auto-generated method stub
+                mp.start();
+                mp.setLooping(true);
+            }
+        });
+    }
+
+
+    public void findView(){
+
+    }
+
+    public void initSettings(){
+
+//        mainActivtiy_main_fragment = new MainActivtiy_main_fragment();
+//        mainActivity_signin_fragment = new MainActivity_signin_fragment();
+//        mainActivity_signup_fragment = new MainActivity_signup_fragment();
+//        getFragmentManager().beginTransaction().add(R.id.activity_main_fragment_relativelayout, mainActivtiy_main_fragment).commit();
+
+        fragmentManager = getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.activity_main_fraglayout,new MainActivtiy_main_fragment(),"main_fragment");
+        fragmentTransaction.commit();
+
+    }
+
+    public void setListener(){
+
     }
 }
--- app/src/main/java/com/xienan/im/MainActivtiy_main_fragment.java
+++ app/src/main/java/com/xienan/im/MainActivtiy_main_fragment.java
@@ -3,12 +3,38 @@ package com.xienan.im;
 import android.animation.Animator;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.YoYo;
 
 /**
  * Created by Administrator on 2016/1/25.
  */
 public class MainActivtiy_main_fragment extends Fragment{
+    private View mMainView;
+    private TextView activity_main_fragment_layout_im;
+    private Button activity_main_fragment_layout_gosignin_submit,activity_main_fragment_layout_gosignup_submit;
+    private MainActivityButtonClick mainActivityButtonClick;
 
+    private View mTarget;
+    private YoYo.YoYoString rope;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        inflater = getActivity().getLayoutInflater();
+        mMainView = inflater.inflate(R.layout.activity_main_fragment,container,false);
+        findViewById();
+
+        initSettings();
+
+        setListener();
+
+        return mMainView;
+    }
 
     @Override
     public Animator onCreateAnimator(int transit, boolean enter, int nextAnim) {
@@ -20,25 +46,45 @@ public class MainActivtiy_main_fragment extends Fragment{
         super.onActivityCreated(savedInstanceState);
     }
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        findViewById();
-
-        initSettings();
-
-        setListener();
     }
 
     public void findViewById(){
+        mainActivityButtonClick = new MainActivityButtonClick();
+        mTarget = mMainView.findViewById(R.id.activity_main_fragment_layout_im);
+//        rope = YoYo.with(Techniques.FadeIn).duration(1000).playOn(mTarget);
+        activity_main_fragment_layout_gosignin_submit = (Button)mMainView.findViewById(R.id.activity_main_fragment_layout_gosignin_submit);
+        activity_main_fragment_layout_gosignup_submit = (Button)mMainView.findViewById(R.id.activity_main_fragment_layout_gosignup_submit);
 
     }
 
     public void initSettings(){
-
+        activity_main_fragment_layout_gosignin_submit.setOnClickListener(mainActivityButtonClick);
+        activity_main_fragment_layout_gosignup_submit.setOnClickListener(mainActivityButtonClick);
     }
 
     public void setListener(){
 
+    }
+
+
+
+    class MainActivityButtonClick implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.activity_main_fragment_layout_gosignin_submit:{
+
+                }
+                case R.id.activity_main_fragment_layout_gosignup_submit:{
+
+                }
+                default:{}
+            }
+        }
     }
 }
